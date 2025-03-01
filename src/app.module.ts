53bc8fd931e17f2e039e7485b01c03a1b9de8023
nikita.azizov <nik.azizoff2016@gmail.com>
2025-03-01 16:21:34 +0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { logger } from './middleware/logger.middleware';
+
+@Module({
+  imports: [UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(logger)
+      .exclude({ path: 'user/*id', method: RequestMethod.PUT })
+      .forRoutes('user');
+  }
+}
